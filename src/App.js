7ef96f8a9043ebc5e6bf019523bc59ad762ccbf1
Scarlet Juskiw <juzkiv.scarlet@gmail.com>
2022-01-18 09:40:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//	src/App.js
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+        		<img src={logo} className="App-logo" alt="logo" />
+        		<p>
+        			Edit <code>src/App.js</code> and save to reload.
+        		</p>
+        		<a
+        			className="App-link"
+        			href="https://reactjs.org"
+					rel="noopener noreferrer"
+        			target="_blank"
+        		>
+        			Learn React
+        		</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
